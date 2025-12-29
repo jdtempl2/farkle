9aa12caa3dab6d2a7059792d994d8f8e21769c8b
Jonah Temple <jonah.d.temple@gmail.com>
2025-12-29 15:01:02 -0500

    Fixed duplicate scores problem. Now scoreDice() will return a dict of the form {idx: [pointsVal, [d0, d1, dN]] where the 2nd list is the list of dice used to form that score.

--- farkle.py
+++ farkle.py
@@ -3,12 +3,13 @@ import random
 
 def scoreDice(dice):
     #  this function will take an arbitrary list of Dice values and return a Dictionary of possible scores
-    #  dict will take the form of 'scores[points] = [list_of_dice]'
-    #  so a roll of [1, 1, 2, 5, 5, 6] will return {100: [1], 200: [1, 1], 50: [5], 100: [5, 5]}
+    #  dict will take the form of 'scores[idx] = [points, [list_of_dice]]'
+    #  so a roll of [1, 1, 2, 5, 5, 6] will return {0: [100, [1]], 1: [200, [1, 1]], 2: [50, [5]], 3: [100, [5, 5]]}
 
     num_dice = len(dice)
     values = [0, 0, 0, 0, 0, 0]  # number of ones, twos, threes, etc
     scores = {}  # will store list of scores & their associated die
+    sidx = 0  # index for scores
     for d in range(num_dice):
         val = dice[d] - 1  # since val is an index, and indices start at 0
         values[val] += 1
@@ -19,55 +20,74 @@ def scoreDice(dice):
         no_mults = False
 
     if values[0] == 0 and values[4] == 0 and no_mults is True:
-        return {0: 0}  # no score if no 1s, 5s, or 3+ multiples
+        return {}  # no score if no 1s, 5s, or 3+ multiples
 
     # look for runs, either 1,2,3,4,5, 2,3,4,5,6, or 1,2,3,4,5,6
     runs_score = 0
     if values[0] > 0 and values[1] > 0 and values[2] > 0 and values[3] > 0 and values[4] > 0:
-        scores[750] = [1, 2, 3, 4, 5]
+        scores[sidx] = [750, [1, 2, 3, 4, 5]]
+        sidx += 1
     if values[5] > 0 and values[1] > 0 and values[2] > 0 and values[3] > 0 and values[4] > 0:
-        scores[750] = [2, 3, 4, 5, 6]
+        scores[sidx] = [750, [2, 3, 4, 5, 6]]
+        sidx += 1
     if len(scores) == 2:  # IE if both runs are present
-        scores[1500] = [1, 2, 3, 4, 5, 6]
+        scores[sidx] = [1500, [1, 2, 3, 4, 5, 6]]
+        sidx += 1
 
     for i in range(6):
         d = i + 1  # the number on the dice
         val = values[i]
         if d == 1:
             if val >= 1:
-                scores[100] = [d]
+                scores[sidx] = [100, [d]]
+                sidx += 1
             if val >= 2:
-                scores[200] = [d, d]
+                scores[sidx] = [200, [d, d]]
+                sidx += 1
             if val >= 3:
-                scores[1000] = [d, d, d]
+                scores[sidx] = [1000, [d, d, d]]
+                sidx += 1
             if val >= 4:
-                scores[2000] = [d, d, d, d]
+                scores[sidx] = [2000, [d, d, d, d]]
+                sidx += 1
             if val >= 5:
-                scores[4000] = [d, d, d, d, d]
+                scores[sidx] = [4000, [d, d, d, d, d]]
+                sidx += 1
             if val == 6:
-                scores[8000] = [d, d, d, d, d, d]
+                scores[sidx] = [8000, [d, d, d, d, d, d]]
+                sidx += 1
         elif d == 5:
             if val >= 1:
-                scores[50] = [d]
+                scores[sidx] = [50, [d]]
+                sidx += 1
             if val >= 2:
-                scores[100] = [d, d]
+                scores[sidx] = [100, [d, d]]
+                sidx += 1
             if val >= 3:
-                scores[d*100] = [d, d, d]
+                scores[sidx] = [d*100, [d, d, d]]
+                sidx += 1
             if val >= 4:
-                scores[d*200] = [d, d, d, d]
+                scores[sidx] = [d*200, [d, d, d, d]]
+                sidx += 1
             if val >= 5:
-                scores[d*400] = [d, d, d, d, d]
+                scores[sidx] = [d*400, [d, d, d, d, d]]
+                sidx += 1
             if val == 6:
-                scores[d*800] = [d, d, d, d, d, d]
+                scores[sidx] = [d*800, [d, d, d, d, d, d]]
+                sidx += 1
         else:
             if val >= 3:
-                scores[d*100] = [d, d, d]
+                scores[sidx] = [d*100, [d, d, d]]
+                sidx += 1
             if val >= 4:
-                scores[d*200] = [d, d, d, d]
+                scores[sidx] = [d*200, [d, d, d, d]]
+                sidx += 1
             if val >= 5:
-                scores[d*400] = [d, d, d, d, d]
+                scores[sidx] = [d*400, [d, d, d, d, d]]
+                sidx += 1
             if val == 6:
-                scores[d*800] = [d, d, d, d, d, d]
+                scores[sidx] = [d*800, [d, d, d, d, d, d]]
+                sidx += 1
 
     return scores
 
